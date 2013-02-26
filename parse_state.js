0aac23152bc746a189808817a4ccad23cee71765
Matt Fenwick <mfenwick100@gmail.com>
2013-02-26 22:00:55 +0100

    working example using stateful parser to match braces, including type of braces ( '(' '[' or '{' ), generates useful error messages

--- parse_state.js
+++ parse_state.js
@@ -196,6 +196,12 @@ function ParserFactory(Type) {
             return good(f(s), xs, null);
         });
     };
+    // how about:
+    //    Parser.getState.bind(function(s) {
+    //        return Parser.putState(f(s));
+    //    });
+    // or:
+    //    Parser.getState.bind(compose(Parser.putState, f))
     
     // (a -> Bool) -> Parser t a -> Parser t a
     Parser.prototype.check = function(p) {
@@ -410,7 +416,9 @@ var Braces = (function() {
         if(this.opens.length === 0) {
             return false;
         }
-        return new Braces(this.opens.slice(0, this.opens.length - 1));
+        var butLast = this.opens.slice(0, this.opens.length - 1),
+            last = this.opens[this.opens.length - 1];
+        return [last, new Braces(butLast)];
     };
     
     return Braces;
@@ -424,39 +432,49 @@ var BlockParser = (function () {
         return a === b[0];
     }
     
-    function openBrace(t) {
+    function pushToken(t) {
         return Parser.updateState(function(bs) {
             return bs.push(t);
         }).seq2R(Parser.pure(t));
     }
     
-    function closeBrace(t) {
+    function matches(open, close) {
+        var o = open[0],
+            c = close[0];
+        var ms = { '{' : '}', '(': ')', '[': ']'};
+        return ms[o] === c;
+    }        
+    
+    function popToken(t) {
         return Parser.getState.bind(function(bs) {
             var newBs = bs.pop();
-            if(newBs) {return Parser.putState(newBs).seq2R(Parser.pure(t));}
-            return Parser.error({message: "unmatched }", brace: t});
+            if(!newBs) {return Parser.error({message: "unmatched brace", brace: t});}
+            var top = newBs[0],
+                rest = newBs[1];
+            if(!top) {alert("fuck me in the ass and call me gay");}
+            if ( matches(top, t) ) {
+                return Parser.putState(rest).seq2R(Parser.pure(t));
+            }
+            return Parser.error({message: 'mismatched braces', open: top, close: t});
         });
     }
     
-    var open = Parser.literal("{", eq).bind(openBrace),
-        close = Parser.literal("}", eq).bind(closeBrace),
+    var OS = {'{': 1, '(': 1, '[': 1},
+        CS = {'}': 1, ')': 1, ']': 1};
+    
+    var open = Parser.item.check(function(t) {return t[0] in OS;}).bind(pushToken),
+        close = Parser.item.check(function(t) {return t[0] in CS;}).bind(popToken),
         normalChar = open.plus(close).not1().fmap(function(t) {return t[0];});
         
     var form = new Parser(function() {}); // forward declaration to allow recursion
     
-    /*var block = Parser.app(
-        function(o, fs, cl) {return {type: 'block', start: o.slice(1), forms: fs, end: cl.slice(1)};},
-        open,
-        form.many0(),
-        close.commit('unmatched {'));
-    */
     var block = open.bind(function(o) {
         return Parser.app(
             function(fs, cl) {
                 return {type: 'block', start: o.slice(1), forms: fs, end: cl.slice(1)};
             },
             form.many0(),
-            close.commit({message: 'unmatched {', brace: o}));
+            close.commit({message: 'unmatched brace', brace: o}));
     });
     
     form.parse = normalChar.plus(block).parse; // set 'form' to its actual value
@@ -468,10 +486,21 @@ var BlockParser = (function () {
 })();
 
 var str = "abc{\nd{derr}ef}g\nh}ib{largh".split(''),
-    line = 1, column = 1, toks = [];
+    toks = addLineCol(str),
+    toks2 = addLineCol("abcde");
 
-for(var i = 0; i < str.length; i++) {
-    toks.push([str[i], line, column]);
-    column++;
-    if(str[i] === '\n') {line++; column = 1;}
+function addLineCol(str) {
+    var toks = [],
+        line = 1, 
+        column = 1;
+    for(var i = 0; i < str.length; i++) {
+        toks.push([str[i], line, column]);
+        column++;
+        if(str[i] === '\n') {line++; column = 1;}
+    }
+    return toks;
+}
+
+function example(str) {
+    return BlockParser.parse(new Braces([]), addLineCol(str));
 }
