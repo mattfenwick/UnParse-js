adf6d14f46be5c501880d27da14ab1d08a26487a
Matt Fenwick <mfenwick100@gmail.com>
2013-12-08 07:07:31 -0500

    remove json example & tests

--- src/testmain.js
+++ src/testmain.js
@@ -3,9 +3,7 @@
 require([
     "test/maybeerror", 
     "test/combinators",
-    "test/cst",
-    "test/examples/json",
-    "test/examples/jsontree"
+    "test/cst"
 ], function($, Q) {
     var mods = Array.prototype.slice.call(arguments);
     mods.map(function(mod, ix) {
