12b7e0bf30f57bfda5617ecacc4431c58907d719
Matt Fenwick <mfenwick100@gmail.com>
2014-03-21 08:52:29 -0400

    ditch bower config; reorganize to node config

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+module.exports = {
+    'maybeerror' : require('./libs/maybeerror.js') ,
+    'combinators': require('./libs/combinators.js'),
+    'cst'        : require('./libs/cst.js')
+};
+
