378625dca6fbf2bfe527ae87e88bee4a57a2d961
Matt Fenwick <mfenwick100@gmail.com>
2016-01-09 09:52:26 -0600

    upgrade version numbers to 0.2.0

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
     'maybeerror' : require('./lib/maybeerror.js') ,
     'combinators': require('./lib/combinators.js'),
     'cst'        : require('./lib/cst.js'),
+    'operators'  : require('./lib/operators.js'),
     
-    '__version__': '0.1.8'
+    '__version__': '0.2.0'
 };
 
