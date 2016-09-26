7a0f9e8396ad97d2a23b58e888c8d3afe5c8799d
Matt Fenwick <mfenwick100@gmail.com>
2016-09-26 09:44:01 -0500

    #25: use ... operator for var-args

--- test/functions.js
+++ test/functions.js
@@ -25,19 +25,6 @@ testModule('functions', function() {
         deepEqual(tripleThenAdd(19), 61);
     });
     
-    test("getArgs", function() {
-        function g() {
-            return F.getArgs(arguments, 0);
-        }
-        function h() {
-            return F.getArgs(arguments, 1);
-        }
-        deepEqual(g(0, 1, 2, 3), [0, 1, 2, 3]);
-        deepEqual(g(), []);
-        deepEqual(h(0, 1, 2, 3), [1, 2, 3]);
-        deepEqual(h(), []);
-    });
-    
     test("first", function() {
         deepEqual(F.first(1, 2), 1);
         deepEqual(F.first(), undefined);
